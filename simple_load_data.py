5706a9b524ba2fa72cde06d7c2a96f127862e678
EvanCornforth <evancornforth@gmail.com>
2017-10-09 20:16:33 +0100

    Made a few changes to simple_load_data

--- simple_load_data.py
+++ simple_load_data.py
@@ -5,3 +5,16 @@ import numpy as np
 mydata = np.loadtxt(fname = 'some_data.csv', delimiter = ',')
 
 print mydata
+
+f = open ('some_data.csv', 'r')
+
+lines = f.readlines()
+
+for x in lines:
+	print x
+
+line = [lines[i].split(',') for i in range(3)] 
+
+for i in range(3):
+	for j in range(4):
+		print line[i][j]
